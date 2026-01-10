8519110b902a18bb65675ef6d3abd81440c09bcb
Jack-by <55187233+Jack-by@users.noreply.github.com>
2026-01-10 17:54:13 +0300

    Initial commit

---
+++ Drivers/uc1698u/uc1698u.h
@@ -0,0 +1,69 @@
+
+
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __UC1698U
+#define __UC1698U
+
+
+#include "main.h"
+#include "stdint.h"
+#include "stm32f4xx_hal.h"
+
+
+#define ADDR_CMD  *(uint8_t*)0x60000000
+#define ADDR_DATA *(uint8_t*)0x60010000
+
+
+
+
+
+
+
+void UC1698_writecommand8(uint8_t com);//command write
+void UC1698_writedata8(uint8_t data);//data write
+
+void init(void);
+void test(void);
+void display_black(void);
+void display_white(void);
+void frame(void);
+void all_v1(void);
+void all_v2(void);
+void text_dot(uint8_t data1, uint8_t data2);
+void delayms(uint32_t t);
+void write_com(uint32_t com);
+void write_data(uint32_t data);
+void Data_processing(uint8_t temp);
+void adj_Contrast(uint8_t Contrast_level);
+void display_Contrast_level(uint8_t number);
+void Write_number(uint8_t x, uint8_t y, const uint8_t *n, uint8_t k);
+void display_address(void);
+void display_gray(void);
+
+void disppic(const uint8_t *pic);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ README.md
@@ -0,0 +1,2 @@
+# uc1698u_160x160_F407
+A working example of the ERC160160-2 display on an STM32F407VGT6
